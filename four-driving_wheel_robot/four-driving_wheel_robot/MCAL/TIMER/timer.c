5a28a433c0e1dd67d413e2c5f648c874ea2e597b
m3adel <mechatronics573@gmail.com>
2023-04-11 15:05:04 +0200

    Update timer.c

--- four-driving_wheel_robot/four-driving_wheel_robot/MCAL/TIMER/timer.c
+++ four-driving_wheel_robot/four-driving_wheel_robot/MCAL/TIMER/timer.c
@@ -5,24 +5,24 @@
  *  Author: MOHAMED ABDELSALAM
  */ 
 
-#include "timer.h"
+#include "timer2.h"
 
 
 void TIMER_init (uint8_t Mode,uint8_t intial_value){
 //choose timer mode
-TCCR0 &= Mode;
+TCCR2 &= Mode;
 //TimerSet initial value
-TCNT0 = intial_value;
+TCNT2 = intial_value;
 }
 void TIMER_start (uint8_t prescaler_value){
 
-TCCR0 |= prescaler_value;  //Set Pre_scaler, for ex:ck(source)/1 to get,
+TCCR2 |= prescaler_value;  //Set Pre_scaler, for ex:ck(source)(8MHz)/8 to get,
 //T(tick) = 1 microseconds
-//Stop after one overflow 256 microseconds
+//Stop after one overflow 250 microseconds
 }
 void TIMER_set(uint8_t intial_value){
 //TimerSet initial value
-TCNT0 = intial_value;
+TCNT2 = intial_value;
 }
 void TIMER_getStatus(uint8_t *value){
 	if ((TIFR & clear) == 0)
@@ -37,31 +37,29 @@ void TIMER_getStatus(uint8_t *value){
 }
 	void TIMER_Stop (void){
 		//Timer Stop
-		TCCR0 = no_clk;	
+		TCCR2 = no_clk;	
 	}
 void Delay(uint32_t milliseconds)
 {
-	TIMER_init (normal,Intial_value);
-	TIMER_start (pres_1);
+	TIMER_init (normal,ticks_250);
+	TIMER_start (pres_8);
 	uint32_t i;
 	uint8_t counter;
 	
 	for(i=0;i< milliseconds ;i++){
 	
-	// N = (1000us)/(256u);  //=3.9 by approx. it will 3
-	// n = ((1000us)-(3numovflow)*(256us))/(1us)  // = 66 ticks
-	//for one millisecond delay iterate 3 times of timer overflow and 232 ticks
+	// N = (1000us)/(250u);  //= 4 without approximation
+	//for one millisecond delay iterate 4 times of timer2 overflow
 	counter = 0;
-	while(counter<3){
+	while(counter<4){
 		
 		while((TIFR & clear) == 0);
+		TIMER_set(ticks_250);
 		TIFR |= clear;
 		counter++;
 	}
-	// instructions time between setting ticks and timer starting count from last overflow is 16us
-	TIMER_set(ticks_214);  // 232ticks - 18us =214ticks, so substitute by 214ticks instead of 232ticks
-	while ((TIFR & clear) == 0);
-	TIFR |= clear;
+	
+
 	}
 	TIMER_Stop ();
 }
