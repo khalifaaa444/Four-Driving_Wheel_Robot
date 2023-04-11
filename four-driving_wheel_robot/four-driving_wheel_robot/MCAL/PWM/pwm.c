a7beb54d8e0f5c7ee9b8622d0921a5b3dff26421
m3adel <mechatronics573@gmail.com>
2023-04-11 15:01:01 +0200

    Update pwm.c

--- four-driving_wheel_robot/four-driving_wheel_robot/MCAL/PWM/pwm.c
+++ four-driving_wheel_robot/four-driving_wheel_robot/MCAL/PWM/pwm.c
@@ -4,3 +4,24 @@
  * Created: 4/6/2023 11:18:12 PM
  *  Author: MOHAMED ABDELSALAM
  */ 
+
+#include "pwm.h"
+
+void PWM_init (uint8_t Mode){
+	//OC0 pin direction
+	DIO_init(OC0_port,OC0_pin,OUT);
+	//choose PWM mode
+	TCCR0 &= Mode;
+	TCCR0 |= Mode;
+}
+void PWM_start (uint8_t duty_percent){
+
+	OCR0 = duty_percent;			//set the duty cycle
+	//R0 = duty_percent;
+	TCCR0 |= pres_1;  //Set Pre_scaler, for ex:ck(8MHz)/1 to get,
+}
+
+void PWM_Stop (void){
+	//Timer Stop
+	TCCR0 = no_clk;
+}
