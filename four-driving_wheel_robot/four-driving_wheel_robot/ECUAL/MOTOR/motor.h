7f21d769af96a144ea67b64b0957684b9b31c949
m3adel <mechatronics573@gmail.com>
2023-04-11 17:40:11 +0200

    Update motor.h

--- four-driving_wheel_robot/four-driving_wheel_robot/ECUAL/MOTOR/motor.h
+++ four-driving_wheel_robot/four-driving_wheel_robot/ECUAL/MOTOR/motor.h
@@ -1,16 +1,25 @@
 /*
- * motor.h
+ * Motors.h
  *
- * Created: 4/6/2023 11:21:47 PM
- *  Author: KHALIFA
+ * Created: 4/11/2023 6:09:10 AM
+ *  Author: ebrahem
  */ 
 
 
-#ifndef MOTOR_H_
-#define MOTOR_H_
+#ifndef MOTORS_H_
+#define MOTORS_H_
+#include "../MCAL/DIO_Interface.h"
 
+void motors_init( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B );
 
+void motors_forward( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B );
 
+void motors_Backward( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B );
 
+void motors_right( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B );
 
-#endif /* MOTOR_H_ */
+void motors_left( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B );
+
+void motors_stop( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B );
+
+#endif /* MOTORS_H_ */
