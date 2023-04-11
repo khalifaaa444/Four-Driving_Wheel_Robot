ee2575cf754dd45ea5b78485acdafd874f27d55a
m3adel <mechatronics573@gmail.com>
2023-04-11 15:06:31 +0200

    Update registers.h

--- four-driving_wheel_robot/four-driving_wheel_robot/UTILITIES/registers.h
+++ four-driving_wheel_robot/four-driving_wheel_robot/UTILITIES/registers.h
@@ -11,10 +11,46 @@
 
 
 
+
 /******************************************************************************/
 // Timer0 Registers
 /******************************************************************************/
 #define TIFR *((volatile uint8_t*)0x58)
 #define TCCR0 *((volatile uint8_t*)0x53)
 #define TCNT0 *((volatile uint8_t*)0x52)
+#define OCR0 *((volatile uint8_t*)0x5C)
+/******************************************************************************/
+// DIO Registers
+/******************************************************************************/
+//PortA Registers
+#define PORTA *((volatile uint8_t*)0x3B)
+#define DDRA *((volatile uint8_t*)0x3A)
+#define PINA *((volatile uint8_t*)0x39)
+//PortB Registers
+#define PORTB *((volatile uint8_t*)0x38)
+#define DDRB *((volatile uint8_t*)0x37)
+#define PINB *((volatile uint8_t*)0x36)
+//PortC Registers
+#define PORTC *((volatile uint8_t*)0x35)
+#define DDRC *((volatile uint8_t*)0x34)
+#define PINC *((volatile uint8_t*)0x33)
+//PortD Registers
+#define PORTD *((volatile uint8_t*)0x32)
+#define DDRD *((volatile uint8_t*)0x31)
+#define PIND *((volatile uint8_t*)0x30)
+/******************************************************************************/
+// Timer1 Registers
+/******************************************************************************/
+#define TIMSK *((volatile uint8_t*)0x59)
+#define TCCR1A *((volatile uint8_t*)0x4F)
+#define TCCR1B *((volatile uint8_t*)0x4E)
+#define TCNT1H *((volatile uint8_t*)0x4D)
+#define TCNT1L *((volatile uint8_t*)0x4C)
+/******************************************************************************/
+// Timer2 Registers
+/******************************************************************************/
+#define TIFR *((volatile uint8_t*)0x58)
+#define TCCR2 *((volatile uint8_t*)0x45)
+#define TCNT2 *((volatile uint8_t*)0x44)
+/******************************************************************************/
 #endif /* REGISTERS_H_ */
