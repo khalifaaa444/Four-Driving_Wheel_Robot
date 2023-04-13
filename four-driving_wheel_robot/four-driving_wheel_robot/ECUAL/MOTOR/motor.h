a01693f0a4079b45105e99c324447b9cf91b94ea
khalifaaa444 <116544385+khalifaaa444@users.noreply.github.com>
2023-04-14 00:10:46 +0200

    Update motor.h

--- four-driving_wheel_robot/four-driving_wheel_robot/ECUAL/MOTOR/motor.h
+++ four-driving_wheel_robot/four-driving_wheel_robot/ECUAL/MOTOR/motor.h
@@ -10,16 +10,16 @@
 #define MOTORS_H_
 #include "../MCAL/DIO_Interface.h"
 
-void motors_init( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B );
+void motors_init         ( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B                );
 
-void motors_forward( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B );
+void motors_forward      ( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B , uint8_t speed);
 
-void motors_Backward( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B );
+void motors_Backward     ( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B ,uint8_t speed );
 
-void motors_right( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B );
+void motors_right        ( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B ,uint8_t speed );
 
-void motors_left( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B );
+void motors_left         ( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B , uint8_t speed);
 
-void motors_stop( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B );
+void motors_stop         ( PIn_name pin_motor1_F , PIn_name pin_motor1_B ,PIn_name pin_motor2_F, PIn_name pin_motor2_B                );
 
 #endif /* MOTORS_H_ */
